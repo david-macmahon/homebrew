0157879949bbeb49d191e3191f7ff9f774f9e1fc
David MacMahon <davidm@astro.berkeley.edu>
2014-02-03 16:54:20 -0800

    Add sha256 entries and a head entry for geda-gaf

--- Library/Formula/geda-gaf.rb
+++ Library/Formula/geda-gaf.rb
@@ -4,11 +4,15 @@ class GedaGaf < Formula
   homepage 'http://www.geda-project.org/'
   url 'http://ftp.geda-project.org/geda-gaf/stable/v1.8/1.8.2/geda-gaf-1.8.2.tar.gz'
   sha1 'c61edc04997fc12398534a346dac32d8fcdabcc1'
+  sha256 'bbf4773aef1b5a51a8d6f4c3fa288c047340cc62dd6e14d7928fcc6e4051b721'
 
   devel do
     url 'http://ftp.geda-project.org/geda-gaf/unstable/v1.9/1.9.0/geda-gaf-1.9.0.tar.gz'
     sha1 '2b6732238ca5ed6693695d737e52aef1bdb2a589'
+    sha256 '6db54587f4ff1798c486d71645abeba5906c582182a07ce1895538e89ef726c6'
   end
+
+  head 'git://git.geda-project.org/geda-gaf'
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
